81169d1b16841dfd469a2e95ffe7fd160f7527ef
José Arévalo <joseaf1002@gmail.com>
2023-04-12 14:00:56 +0200

    Add loader

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Col } from 'antd';
+import { Col, Spin, Space } from 'antd';
 import Searcher from './components/Searcher';
 import PokemonList from './components/PokemonList';
 import { getPokemon } from './api';
-import { getPokemonWithDetails } from './actions';
+import { getPokemonWithDetails, setLoading } from './actions';
 import logo from './statics/logo.svg';
 import './App.css';
 
 function App() {
   const pokemons = useSelector(state => state.pokemons);
+  const loading = useSelector(state => state.loading);
   const dispatch = useDispatch()
 
   useEffect(() => {
     const fetchPokemons = async () => {
+      dispatch(setLoading(true))
       const pokemonsRes = await getPokemon();
       dispatch(getPokemonWithDetails(pokemonsRes));
+      dispatch(setLoading(false))
     };
 
     fetchPokemons();
@@ -29,7 +32,13 @@ function App() {
       <Col span={8} offset={8}>
         <Searcher />
       </Col>
-      <PokemonList pokemons={pokemons} />
+
+      {loading ? <Col offset={12}>
+          <Spin size="large" />
+      </Col>
+      : <PokemonList pokemons={pokemons} />
+      }
+
     </div>
   );
 }
